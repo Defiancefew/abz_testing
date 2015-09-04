b0e83c79c0e7aa7f4fc60a60d62c67cdc74c95ad
Defiancefew <Severancepwns@gmail.com>
2015-09-04 22:56:53 +0300

    third commit

--- src/js/common.js
+++ src/js/common.js
@@ -1,14 +1,8 @@
-(function () {
+;(function () {
 
 var app = angular
-				.module('app', [])
-				.controller('MainCtrl', MainCtrl);
-
-// app.factory("auth", ["$resource", function($resource){
-
-//  return $resource("http://504080.com/api/v1/services/categories");
-
-// }]);
+	.module('app', [])
+	.controller('MainCtrl', MainCtrl);
 
 function MainCtrl ($scope, $http) {
 
@@ -24,24 +18,42 @@ $http.get(url).success(function(data, status, headers, config) {
 
 }).error(function(data, status, headers, config){
 
-	var token = data.error;
+$scope.tokenMessage = data.error;
 
-	$scope.tokenMessage = token;
+	if(status == "401" || "500"){
 
+		errorInitiate();
 
-	// if(status == "401"){
-	// 	$scope.message = "Unauthorized";
-	// 	$scope.description = "Access token is missing or expired. Please login or signup to use our service";
-	// 	$scope.code = "400";
-	// }
-	// else if(status == "500"){
-	// 	$scope.message = "Server Error";
-	// 	$scope.description = "Something happens on serverside. Please contact administrator or try again later";
-	// 	$scope.code = "500";
-	// }
+		}
 
 	});
 }
+
+var popup = document.querySelector(".popup"),
+		overlay = document.querySelector(".overlay"),
+		link = document.querySelector(".popup__close"),
+		service = document.querySelector(".item__wrapper");
+
+link.addEventListener("click", function(e){
+		toggleMenus(e);
+});
+
+function errorInitiate(){
+
+	popup.classList.remove("hidden");
+	overlay	.classList.remove("hidden");
+	service.style.display = 'none';
+	
+}
+
+function toggleMenus(e){
+
+	e.preventDefault();
+	popup.classList.toggle("hidden");
+	overlay.classList.toggle("hidden");
+}
+
+
 
 })();
 
